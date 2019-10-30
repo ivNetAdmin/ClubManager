6f791e9b926260a7491f4f41fa79cda64e9fad65
bp <bp@ivnet.co.uk>
2019-10-30 16:18:34 +0000

    Add Member Form

--- ClubManager/ClientApp/src/app/add-member/add-member.component.ts
+++ ClubManager/ClientApp/src/app/add-member/add-member.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-add-member',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AddMemberComponent {
 
+  @ViewChild("memberForm") memberForm;
+
+  setMemberType(type) {
+    console.log(type);
+  }
 }
