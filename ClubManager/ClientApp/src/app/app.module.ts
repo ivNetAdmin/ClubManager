ab6585297b335e38ec7818f86b0c27e348cf5673
bp <bp@ivnet.co.uk>
2019-10-30 13:33:18 +0000

    List & Add member Navigation

--- ClubManager/ClientApp/src/app/app.module.ts
+++ ClubManager/ClientApp/src/app/app.module.ts
@@ -8,20 +8,26 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { FetchMembersComponent } from './fetch-members/fetch-members.component';
+import { AddMemberComponent } from './add-member/add-member.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    SideBarComponent
+    SideBarComponent,
+    FetchMembersComponent,
+    AddMemberComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' }
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'fetch-members', component: FetchMembersComponent },
+      { path: 'add-member', component: AddMemberComponent }
     ])
   ],
   providers: [],
